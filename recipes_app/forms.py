d020b4958b919b5b2329379bafbe6cf50a68b7f9
jp-cortes <code_alias@outlook.com>
2024-11-22 02:26:38 +0200

    updating styles the recipes form

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Recipe
 
 class RecipeForm(forms.Form):
-    name = forms.CharField(max_length=100, label="name", widget=forms.TextInput(attrs={"placeholder" : "Pizza Margarita"}))
+    name = forms.CharField(max_length=100, label="name", widget=forms.TextInput(attrs={"placeholder" : "Pizza Margarita", "style": "border-radius: 0.25rem; padding: 4px; width: 100%;"}))
     images = forms.ImageField(label="images", required=False)
-    ingredients = forms.CharField(label="ingredients", widget=forms.TextInput(attrs={"placeholder" : "100gr white cheese, 2 eggs, ..."}))
-    preparation = forms.CharField(label="preparation", widget=forms.Textarea(attrs={"placeholder" : "Mix all ingredients..."}))
+    ingredients = forms.CharField(label="ingredients", widget=forms.TextInput(attrs={"placeholder" : "100gr white cheese, 2 eggs, ...", "style": "border-radius: 0.25rem; padding: 4px; width: 100%;"}))
+    preparation = forms.CharField(label="preparation", widget=forms.Textarea(attrs={"placeholder" : "Mix all ingredients...", "style": "border-radius: 0.25rem; padding: 4px; width: 100%;"}))
 
 
     def save(self):
