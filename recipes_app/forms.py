6b8f8fe48ddc4fae5217001ac934071b40dd0f92
jp-cortes <code_alias@outlook.com>
2024-11-24 02:57:15 +0200

    add styles to login.html

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Recipe
 
 class RecipeForm(forms.Form):
-    name = forms.CharField(max_length=100, label="name", widget=forms.TextInput(attrs={"placeholder" : "Pizza Margarita", "style": "border-radius: 0.25rem; padding: 4px; width: 100%;"}))
+    name = forms.CharField(max_length=100, label="name", widget=forms.TextInput(attrs={"placeholder" : "Pizza Margarita", "class": "w-full rounded-lg p-2"}))
     images = forms.ImageField(label="images", required=False)
-    ingredients = forms.CharField(label="ingredients", widget=forms.TextInput(attrs={"placeholder" : "100gr white cheese, 2 eggs, ...", "style": "border-radius: 0.25rem; padding: 4px; width: 100%;"}))
-    preparation = forms.CharField(label="preparation", widget=forms.Textarea(attrs={"placeholder" : "Mix all ingredients...", "style": "border-radius: 0.25rem; padding: 4px; width: 100%;"}))
+    ingredients = forms.CharField(label="ingredients", widget=forms.TextInput(attrs={"placeholder" : "100gr white cheese, 2 eggs, ...", "class": "w-full rounded-lg p-2"}))
+    preparation = forms.CharField(label="preparation", widget=forms.Textarea(attrs={"placeholder" : "Mix all ingredients...", "class": "w-full rounded-lg p-2"}))
 
 
     def save(self):
