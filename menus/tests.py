6433b9c8950bc7b763997861060c5b2244eeb262
jp-cortes <code_alias@outlook.com>
2025-02-16 16:33:38 +0200

    unit testing for recipes & menus

--- menus/tests.py
+++ menus/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class MenuRecipeTest(TestCase):
+
+    def test_no_logged_user_should_redirect(self):
+        url = reverse("my_menu")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url,"/users/login/?next=/menus/my-menu")
+
+    def test_logged_user_should_redirect(self):
+        url = reverse("my_menu")
+        user = get_user_model().objects.create(username="Test")
+        self.client.force_login(user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
--- recipes_app/tests.py
+++ recipes_app/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Recipe, Category
+
+
+class RecipeListViewTest(TestCase):
+    
+    def test_should_return_200(self):
+        url = reverse("all_recipes")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["recipes"].count(), 0)
+
+    def test_should_return_200_with_recipes(self):
+        url = reverse("all_recipes")
+        category_instace = Category.objects.create(
+            title="test category",
+            images="test category image",
+            created_on="test category date"
+        )
+        Recipe.objects.create(
+            name="test", 
+            images="test-image", 
+            ingredients="test ingredients", 
+            preparation="test preparation",
+            category=category_instace,
+            created_on="test date"
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["recipes"].count(), 1)
